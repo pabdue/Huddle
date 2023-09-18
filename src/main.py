e15fa7df3a40aeff1d9f8dd11e33543b3cb7b369
Pablo Duenas <pabloduenas64@gmail.com>
2023-09-17 17:36:33 -0700

    update code base

--- src/main.py
+++ src/main.py
@@ -2,3 +2,15 @@
 # Group: infiniteLoop-JPA
 # Names: Julia Ybanez, Pablo Duenas, Alondra Marin
 # Fall 2023
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/huddle')
+def huddleMessage():
+    return "Welcome to Huddle!"
+
+if __name__ == '__main__':
+    app.run()
