6fe91bbb530cdf6b0d3100a399dfda8186576ade
Pablo Dueñas <pabloduenas@b98-aruba-authenticated-10-110-141-213.wlan.cpp.edu>
2023-10-17 13:58:42 -0700

    add configurations to views and url

--- Huddle_app/views.py
+++ Huddle_app/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
+def huddle_home(request):
+    return render(request, 'index.html')
+
+def huddle_group(request):
+    return render(request, 'huddle_page.html')
