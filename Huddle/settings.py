a3fe18ee8080f256d00ae39f146ff66d365d2e9f
Julia Ybanez <ybanez@cpp.edu>
2023-11-09 13:10:23 -0800

    initial set up of db

--- Huddle/settings.py
+++ Huddle/settings.py
@@ -74,11 +74,16 @@ WSGI_APPLICATION = "Huddle.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'huddledb',
+        'USER': 'postgres',
+        'PASSWORD': 'Huddle123',
+        'HOST': 'localhost', 
+        'PORT': '5432',      
     }
 }
+
 
 
 # Password validation
