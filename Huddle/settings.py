d0e0119b93b626eef9068c79a32754675b787a7e
Julia Ybanez <ybanez@cpp.edu>
2023-11-09 18:41:45 -0800

    changed db to original

--- Huddle/settings.py
+++ Huddle/settings.py
@@ -75,12 +75,18 @@ WSGI_APPLICATION = "Huddle.wsgi.application"
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'huddledb',
-        'USER': 'postgres',
-        'PASSWORD': 'Huddle123',
-        'HOST': 'localhost', 
-        'PORT': '5432',      
+
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",     
+        
+    #
+    #    'ENGINE': 'django.db.backends.postgresql',
+    #    'NAME': 'huddledb',
+    #    'USER': 'postgres',
+    #    'PASSWORD': 'Huddle123',
+    #    'HOST': 'localhost', 
+    #    'PORT': '5432', 
+        
     }
 }
 
