3b533d35d4d0d059b56129a72f7f4a2df13ab777
Pablo Dueñas <pabloduenas@Pablos-MBP.home>
2023-12-02 12:18:05 -0800

    working on it

--- Huddle/urls.py
+++ Huddle/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('Huddle_app.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
 ]
