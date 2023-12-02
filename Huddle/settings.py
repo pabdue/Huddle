496e2e81bfe14dca19228516dca636dc98b1d3f7
Pablo Dueñas <pabloduenas@Pablos-MBP.home>
2023-12-02 14:55:17 -0800

    working on it

--- Huddle/settings.py
+++ Huddle/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-d30e@pj7e)(2=@@h=l+lo*ags+_y^e!a_09dp6mx^@(1b!brsq
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['ec2-54-219-123-237.us-west-1.compute.amazonaws.com', 'huddlgroup.com']
+ALLOWED_HOSTS = ['ec2-54-219-123-237.us-west-1.compute.amazonaws.com', 'huddlgroup.com', '127.0.0.1']
 
 
 # Application definition
@@ -90,8 +90,8 @@ DATABASES = {
         'NAME': 'huddledb',
         'USER': 'postgres',
         'PASSWORD': 'Huddle123',
-        'HOST': 'huddledb.cjujdmuufeks.us-west-1.rds.amazonaws.com',
-        #'HOST': 'localhost', 
+        #'HOST': 'huddledb.cjujdmuufeks.us-west-1.rds.amazonaws.com',
+        'HOST': 'localhost', 
         'PORT': '5432', 
 
     }
