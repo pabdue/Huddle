f4f3cf8b61175e9a5955229926e2a2cc2304b1a2
Pablo Dueñas <pabloduenas@b98-aruba-authenticated-10-110-140-108.wlan.cpp.edu>
2023-12-04 09:22:08 -0800

    config Task table in DB

--- Huddle_app/models.py
+++ Huddle_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from .models import HuddleGroup
 
 # Create your models here.
 class Account(models.Model):
@@ -21,12 +22,11 @@ class HuddleGroup(models.Model):
         return self.name
     
 class Task(models.Model):
-    huddle_group = models.ForeignKey('HuddleGroup', on_delete=models.CASCADE, related_name='tasks')
+    huddle_group = models.ForeignKey(HuddleGroup, on_delete=models.CASCADE, related_name='tasks')
     name = models.CharField(max_length=100)
     description = models.TextField(max_length=1000)
-    people_assigned = models.TextField(max_length=100)  # Assuming a comma-separated list of people assigned
-    deadline = models.DateTimeField(default=timezone.now)  # Set default to current date and time
-
+    people_assigned = models.TextField(max_length=100)
+    deadline = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
         return self.name
