3ac88d1316bc4b471753240a3983472362522ba0
Pablo Dueñas <pabloduenas@b98-aruba-authenticated-10-110-140-108.wlan.cpp.edu>
2023-12-04 18:41:17 -0800

    add members function

--- Huddle_app/views.py
+++ Huddle_app/views.py
@@ -79,7 +79,7 @@ def huddle_group(request, huddle_group_id):
 
         # Get all members associated with the huddle group
         members = HuddleGroupMembers.objects.filter(huddlegroup=huddle_group).select_related('account')
-        other_members = huddle_group.members.all()
+        other_members = huddle_group.members.all().select_related('account')
 
         tasks = Task.objects.filter(huddle_group=huddle_group)
 
